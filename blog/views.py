c132adcfbcae655fdf7c054f803d6f11258d7898
Nimesh Pareek <72460673+nimeshpareek@users.noreply.github.com>
2023-08-10 10:39:55 +0530

    Added files via upload
    First Commit

---
+++ blog/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Nimesh',
+        'title': 'Atomic Python',
+        'content': 'self-help',
+        'date_posted': '09-08-2023'
+    },
+    {
+        'author': 'Sudhir',
+        'title': 'Time Waste',
+        'content': 'Waste',
+        'date posted': '08-08-2023'
+    }
+]
+
+def home(request):
+    # we can pass the above data to the dictionary and above dictionary name is context
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'}) # here the third argument is that we are passing 
+    # something in title like in this case it would be Django Blog - About and if we do not pass anyting then it
+    # will take the else condition which is Django Blog
